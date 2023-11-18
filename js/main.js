08bb67d9796f8502f1931eaf3b07c5bd4e553ad6
unknown <ghkdlxm3565@naver.com>
2023-11-18 22:31:56 +0900

    Design(UI,UX)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
-// map_api
-let mapContainer = document.getElementById('map'), 
-mapOption = { 
-    center: new kakao.maps.LatLng(37.481032156483685, 126.88643021622362), // 지도의 중심좌표
-    level: 3 // 지도의 확대 레벨
-};
+$(document).ready(function(){
 
-let map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-// 마커가 표시될 위치
-let markerPosition  = new kakao.maps.LatLng(37.481032156483685, 126.88643021622362); 
-
-// 마커 생성
-let marker = new kakao.maps.Marker({
-position: markerPosition
+    // section_1_main_slide
+    $('.section_1').slick({
+        prevArrow: '.main_pager .bottom_nav li',
+        nextArrow: '.main_pager .bottom_nav li',
+        // appendDots: '.main_pager .bottom_nav li',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        fade: true
+    });
+    
+    // section_3_slide
+    
+    
+    // map_api
+    let mapContainer = document.getElementById('map'), 
+        mapOption = { 
+            center: new kakao.maps.LatLng(37.481032156483685, 126.88643021622362), // 지도의 중심좌표
+            level: 3 // 지도의 확대 레벨
+        };
+    
+    let map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    
+    // 마커가 표시될 위치
+    let markerPosition  = new kakao.maps.LatLng(37.481032156483685, 126.88643021622362); 
+    
+    // 마커 생성
+    let marker = new kakao.maps.Marker({
+        position: markerPosition
+    });
+    
+    marker.setMap(map);
+    
 });
-
-marker.setMap(map);   
